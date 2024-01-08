48baf13cae34680d60e71666da7d53efe2113d86
Mendy Berger <12537668+MendyBerger@users.noreply.github.com>
2024-01-08 16:54:59 -0500

    Turn crate into lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,12 @@
 #![feature(assert_matches)]
 
-use crate::to_wit::ToWitSyntax;
-
-mod to_wit;
-mod translations;
-
-fn main() -> anyhow::Result<()> {
-    let webidl_code = include_str!("./web.idl");
-    println!("{webidl_code}");
-    let webidl = weedle::parse(&webidl_code)?;
-    let wit = translations::webidl_to_wit(webidl)?;
-    let wit_code = wit.to_wit_syntax(&wit)?;
-    println!("{wit_code}");
-    Ok(())
-}
+pub mod to_wit;
+pub mod translations;
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::to_wit::ToWitSyntax;
     use std::{fs, path::Path};
 
     fn compare(path: &str) {
