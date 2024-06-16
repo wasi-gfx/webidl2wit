907f604878aec7c91d16b23c22ebd8a0abad0704
MendyBerger <12537668+MendyBerger@users.noreply.github.com>
2024-06-15 23:21:00 -0400

    Use a state object

--- src/translations.rs
+++ src/translations.rs
@@ -4,8 +4,6 @@ use heck::{ToKebabCase, ToPascalCase};
 use itertools::Itertools;
 use weedle::{Definition, Definitions as WebIdlDefinitions};
 
-use crate::types_::wi2w_type;
-
 /// conversion options.
 #[derive(Clone, Debug)]
 pub struct ConversionOptions {
@@ -22,14 +20,20 @@ impl Default for ConversionOptions {
         }
     }
 }
+pub(super) struct State<'a> {
+    pub interface: wit_encoder::Interface,
+    pub mixins: HashMap<String, Vec<weedle::interface::InterfaceMember<'a>>>,
+}
 
 pub fn webidl_to_wit(
     webidl: WebIdlDefinitions,
     options: ConversionOptions,
 ) -> anyhow::Result<wit_encoder::Package> {
     let mut package = wit_encoder::Package::new(options.package_name);
-    let mut interface = options.interface;
-    let mut mixins: HashMap<String, Vec<weedle::interface::InterfaceMember<'_>>> = HashMap::new();
+    let mut state = State {
+        interface: options.interface,
+        mixins: HashMap::new(),
+    };
 
     for item in webidl {
         match item {
@@ -42,16 +46,12 @@ pub fn webidl_to_wit(
                     .into_iter()
                     .map(|member| mixin_to_interface_member(member))
                     .collect_vec();
-                mixins.insert(mixin.identifier.0.to_string(), members);
+                state.mixins.insert(mixin.identifier.0.to_string(), members);
             }
             Definition::Namespace(_) => todo!(),
             Definition::PartialInterface(wi_interface) => {
                 let resource_name = ident_name(wi_interface.identifier.0);
-                interface_members_to_functions(
-                    &mut interface,
-                    &resource_name,
-                    &wi_interface.members.body,
-                )?;
+                state.interface_members_to_functions(&resource_name, &wi_interface.members.body)?;
             }
             Definition::PartialInterfaceMixin(_) => todo!(),
             Definition::PartialDictionary(_) => todo!(),
@@ -59,14 +59,15 @@ pub fn webidl_to_wit(
             Definition::IncludesStatement(mixin) => {
                 let mixin_name = mixin.rhs_identifier.0.to_string();
                 let resource_name = ident_name(mixin.lhs_identifier.0);
-                let mixin = mixins.get(&mixin_name).unwrap();
-                interface_members_to_functions(&mut interface, &resource_name, mixin)?;
+                // todo: can we get rid of this clone?
+                let mixin = state.mixins.get(&mixin_name).unwrap().clone();
+                state.interface_members_to_functions(&resource_name, &mixin)?;
             }
             Definition::Implements(_) => todo!(),
             Definition::Typedef(wi_type) => {
-                let type_ = wi2w_type(&mut interface, &wi_type.type_.type_, false)?;
+                let type_ = state.wi2w_type(&wi_type.type_.type_, false)?;
                 let type_def = wit_encoder::TypeDef::type_(ident_name(wi_type.identifier.0), type_);
-                interface.type_def(type_def);
+                state.interface.type_def(type_def);
             }
             Definition::Interface(wi_interface) => {
                 let resource = wit_encoder::Resource::empty();
@@ -75,21 +76,17 @@ pub fn webidl_to_wit(
                     resource_name.clone(),
                     wit_encoder::TypeDefKind::Resource(resource),
                 );
-                interface.type_def(type_def);
-                interface_members_to_functions(
-                    &mut interface,
-                    &resource_name,
-                    &wi_interface.members.body,
-                )?;
+                state.interface.type_def(type_def);
+                state.interface_members_to_functions(&resource_name, &wi_interface.members.body)?;
             }
             Definition::Dictionary(dict) => {
                 let fields = dict.members.body.iter().map(|mem| {
                     let name = ident_name(mem.identifier.0);
-                    let ty = wi2w_type(&mut interface, &mem.type_, mem.required.is_none()).unwrap();
+                    let ty = state.wi2w_type(&mem.type_, mem.required.is_none()).unwrap();
                     (name, ty)
                 });
                 let type_def = wit_encoder::TypeDef::record(ident_name(dict.identifier.0), fields);
-                interface.type_def(type_def);
+                state.interface.type_def(type_def);
             }
             Definition::Enum(e) => {
                 let cases = e
@@ -99,33 +96,35 @@ pub fn webidl_to_wit(
                     .iter()
                     .map(|case| wit_encoder::EnumCase::new(ident_name(case.0)));
                 let type_def = wit_encoder::TypeDef::enum_(ident_name(e.identifier.0), cases);
-                interface.type_def(type_def);
+                state.interface.type_def(type_def);
             }
         }
     }
 
-    package.interface(interface);
+    package.interface(state.interface);
 
     Ok(package)
 }
 
-fn function_args(
-    interface: &mut wit_encoder::Interface,
-    args: &weedle::argument::ArgumentList,
-) -> anyhow::Result<wit_encoder::Params> {
-    Ok(args
-        .list
-        .iter()
-        .map(|arg| match arg {
-            weedle::argument::Argument::Variadic(_) => todo!(),
-            weedle::argument::Argument::Single(arg) => {
-                let name = ident_name(arg.identifier.0);
-                let optional = arg.optional.is_some();
-                let type_ = wi2w_type(interface, &arg.type_.type_, optional).unwrap();
-                (name, type_)
-            }
-        })
-        .collect())
+impl<'a> State<'a> {
+    fn function_args(
+        &mut self,
+        args: &weedle::argument::ArgumentList,
+    ) -> anyhow::Result<wit_encoder::Params> {
+        Ok(args
+            .list
+            .iter()
+            .map(|arg| match arg {
+                weedle::argument::Argument::Variadic(_) => todo!(),
+                weedle::argument::Argument::Single(arg) => {
+                    let name = ident_name(arg.identifier.0);
+                    let optional = arg.optional.is_some();
+                    let type_ = self.wi2w_type(&arg.type_.type_, optional).unwrap();
+                    (name, type_)
+                }
+            })
+            .collect())
+    }
 }
 
 pub(super) fn ident_name(src: &str) -> wit_encoder::Ident {
@@ -177,83 +176,86 @@ fn mixin_to_interface_member(
     }
 }
 
-fn interface_members_to_functions<'a>(
-    interface: &mut wit_encoder::Interface,
-    resource_name: &wit_encoder::Ident,
-    members: impl IntoIterator<Item = &'a weedle::interface::InterfaceMember<'a>>,
-) -> anyhow::Result<()> {
-    let mut functions = Vec::new();
-    for member in members {
-        match member {
-            weedle::interface::InterfaceMember::Const(_) => todo!(),
-            weedle::interface::InterfaceMember::Iterable(_) => todo!(),
-            weedle::interface::InterfaceMember::AsyncIterable(_) => todo!(),
-            weedle::interface::InterfaceMember::Maplike(_) => todo!(),
-            weedle::interface::InterfaceMember::Setlike(_) => todo!(),
-            weedle::interface::InterfaceMember::Stringifier(_) => todo!(),
-            weedle::interface::InterfaceMember::Attribute(attr) => {
-                let attr_name = ident_name(attr.identifier.0);
-                let setter_name = format!("set-{attr_name}");
-                let attr_type = wi2w_type(interface, &attr.type_.type_, false)?;
+impl<'a> State<'a> {
+    fn interface_members_to_functions<'b>(
+        &mut self,
+        resource_name: &'b wit_encoder::Ident,
+        members: impl IntoIterator<Item = &'b weedle::interface::InterfaceMember<'b>>,
+    ) -> anyhow::Result<()> {
+        let mut functions = Vec::new();
+        for member in members {
+            match member {
+                weedle::interface::InterfaceMember::Const(_) => todo!(),
+                weedle::interface::InterfaceMember::Iterable(_) => todo!(),
+                weedle::interface::InterfaceMember::AsyncIterable(_) => todo!(),
+                weedle::interface::InterfaceMember::Maplike(_) => todo!(),
+                weedle::interface::InterfaceMember::Setlike(_) => todo!(),
+                weedle::interface::InterfaceMember::Stringifier(_) => todo!(),
+                weedle::interface::InterfaceMember::Attribute(attr) => {
+                    let attr_name = ident_name(attr.identifier.0);
+                    let setter_name = format!("set-{attr_name}");
+                    let attr_type = self.wi2w_type(&attr.type_.type_, false)?;
 
-                let mut getter = match attr.modifier {
-                    Some(weedle::interface::StringifierOrInheritOrStatic::Static(_)) => {
-                        wit_encoder::ResourceFunc::static_(attr_name.clone())
-                    }
-                    _ => wit_encoder::ResourceFunc::method(attr_name.clone()),
-                };
-                getter.results(attr_type.clone());
-                functions.push(getter);
-                if attr.readonly.is_none() {
-                    let mut setter = match attr.modifier {
+                    let mut getter = match attr.modifier {
                         Some(weedle::interface::StringifierOrInheritOrStatic::Static(_)) => {
-                            wit_encoder::ResourceFunc::static_(setter_name)
+                            wit_encoder::ResourceFunc::static_(attr_name.clone())
                         }
-                        _ => wit_encoder::ResourceFunc::method(setter_name),
+                        _ => wit_encoder::ResourceFunc::method(attr_name.clone()),
                     };
-                    setter.params((attr_name, attr_type));
-                    functions.push(setter);
+                    getter.results(attr_type.clone());
+                    functions.push(getter);
+                    if attr.readonly.is_none() {
+                        let mut setter = match attr.modifier {
+                            Some(weedle::interface::StringifierOrInheritOrStatic::Static(_)) => {
+                                wit_encoder::ResourceFunc::static_(setter_name)
+                            }
+                            _ => wit_encoder::ResourceFunc::method(setter_name),
+                        };
+                        setter.params((attr_name, attr_type));
+                        functions.push(setter);
+                    }
+                }
+                weedle::interface::InterfaceMember::Constructor(constructor) => {
+                    let mut function = wit_encoder::ResourceFunc::constructor();
+                    function.params(self.function_args(&constructor.args.body)?);
+                    functions.push(function);
+                }
+                weedle::interface::InterfaceMember::Operation(operation) => {
+                    let function_name = ident_name(operation.identifier.unwrap().0);
+                    let mut function = match operation.modifier {
+                        Some(weedle::interface::StringifierOrStatic::Static(_)) => {
+                            wit_encoder::ResourceFunc::static_(function_name)
+                        }
+                        _ => wit_encoder::ResourceFunc::method(function_name),
+                    };
+
+                    function.params(self.function_args(&operation.args.body)?);
+
+                    let results = match &operation.return_type {
+                        weedle::types::ReturnType::Undefined(_) => wit_encoder::Results::empty(),
+                        weedle::types::ReturnType::Type(type_) => {
+                            self.wi2w_type(&type_, false)?.into()
+                        }
+                    };
+                    function.results(results);
+                    functions.push(function);
                 }
             }
-            weedle::interface::InterfaceMember::Constructor(constructor) => {
-                let mut function = wit_encoder::ResourceFunc::constructor();
-                function.params(function_args(interface, &constructor.args.body)?);
-                functions.push(function);
-            }
-            weedle::interface::InterfaceMember::Operation(operation) => {
-                let function_name = ident_name(operation.identifier.unwrap().0);
-                let mut function = match operation.modifier {
-                    Some(weedle::interface::StringifierOrStatic::Static(_)) => {
-                        wit_encoder::ResourceFunc::static_(function_name)
-                    }
-                    _ => wit_encoder::ResourceFunc::method(function_name),
-                };
-
-                function.params(function_args(interface, &operation.args.body)?);
-
-                let results = match &operation.return_type {
-                    weedle::types::ReturnType::Undefined(_) => wit_encoder::Results::empty(),
-                    weedle::types::ReturnType::Type(type_) => {
-                        wi2w_type(interface, &type_, false)?.into()
-                    }
-                };
-                function.results(results);
-                functions.push(function);
-            }
         }
+
+        let resource = self
+            .interface
+            .type_defs_mut()
+            .iter_mut()
+            .find(|td| td.name() == resource_name)
+            .expect("Resource not found");
+        let resource = match resource.kind_mut() {
+            wit_encoder::TypeDefKind::Resource(resource) => resource,
+            _ => panic!("Not a resource"),
+        };
+
+        resource.funcs_mut().extend(functions);
+
+        Ok(())
     }
-
-    let resource = interface
-        .type_defs_mut()
-        .iter_mut()
-        .find(|td| td.name() == resource_name)
-        .expect("Resource not found");
-    let resource = match resource.kind_mut() {
-        wit_encoder::TypeDefKind::Resource(resource) => resource,
-        _ => panic!("Not a resource"),
-    };
-
-    resource.funcs_mut().extend(functions);
-
-    Ok(())
 }
--- src/types_.rs
+++ src/types_.rs
@@ -2,141 +2,145 @@ use std::collections::BTreeMap;
 
 use itertools::Itertools;
 
-use crate::translations::ident_name;
+use crate::translations::{ident_name, State};
 
-// WebIdl 2 Wit
-pub fn wi2w_type(
-    interface: &mut wit_encoder::Interface,
-    wi: &weedle::types::Type,
-    optional: bool,
-) -> anyhow::Result<wit_encoder::Type> {
-    match wi {
-        weedle::types::Type::Single(weedle::types::SingleType::NonAny(type_)) => {
-            wi_non_any2w(interface, type_, optional)
-        }
-        weedle::types::Type::Single(weedle::types::SingleType::Any(_any)) => todo!(),
-        weedle::types::Type::Union(union) => {
-            // using a HashSet to get rid of types that are different in WebIDL but are the same in wit.
-            // e.g. `(long or DOMString or ByteString)` should not have two sting options.
-
-            let cases = union
-                .type_
-                .body
-                .list
-                .iter()
-                .map(|type_| match type_ {
-                    weedle::types::UnionMemberType::Single(type_) => {
-                        let type_ = wi_non_any2w(interface, &type_.type_, false).unwrap();
-                        let type_name = type_.to_string();
-                        let type_name = clean_generic(type_name);
-                        (type_name, type_)
-                    }
-                    weedle::types::UnionMemberType::Union(_) => todo!(),
-                })
-                .collect::<BTreeMap<_, _>>();
-
-            // Only create `Variant` if there's more than one type.
-            if cases.len() == 1 {
-                let (_, type_) = cases.into_iter().next().unwrap();
-                return Ok(type_);
+impl<'a> State<'a> {
+    // WebIdl 2 Wit
+    pub fn wi2w_type(
+        &mut self,
+        wi: &weedle::types::Type,
+        optional: bool,
+    ) -> anyhow::Result<wit_encoder::Type> {
+        match wi {
+            weedle::types::Type::Single(weedle::types::SingleType::NonAny(type_)) => {
+                self.wi_non_any2w(type_, optional)
             }
+            weedle::types::Type::Single(weedle::types::SingleType::Any(_any)) => todo!(),
+            weedle::types::Type::Union(union) => {
+                // using a HashSet to get rid of types that are different in WebIDL but are the same in wit.
+                // e.g. `(long or DOMString or ByteString)` should not have two sting options.
 
-            let variant_name = cases
-                .iter()
-                .map(|(name, _)| name.to_owned())
-                .collect_vec()
-                .join("-or-");
-            let variant_name = ident_name(&variant_name);
+                let cases = union
+                    .type_
+                    .body
+                    .list
+                    .iter()
+                    .map(|type_| match type_ {
+                        weedle::types::UnionMemberType::Single(type_) => {
+                            let type_ = self.wi_non_any2w(&type_.type_, false).unwrap();
+                            let type_name = type_.to_string();
+                            let type_name = clean_generic(type_name);
+                            (type_name, type_)
+                        }
+                        weedle::types::UnionMemberType::Union(_) => todo!(),
+                    })
+                    .collect::<BTreeMap<_, _>>();
 
-            if interface
-                .type_defs()
-                .iter()
-                .all(|dt| dt.name() != &variant_name)
-            {
-                interface.type_def({
-                    let cases = cases
-                        .into_iter()
-                        .map(|(name, case)| wit_encoder::VariantCase::value(name, case))
-                        .collect_vec();
-                    let variant = wit_encoder::TypeDef::variant(variant_name.clone(), cases);
-                    variant
-                });
+                // Only create `Variant` if there's more than one type.
+                if cases.len() == 1 {
+                    let (_, type_) = cases.into_iter().next().unwrap();
+                    return Ok(type_);
+                }
+
+                let variant_name = cases
+                    .iter()
+                    .map(|(name, _)| name.to_owned())
+                    .collect_vec()
+                    .join("-or-");
+                let variant_name = ident_name(&variant_name);
+
+                if self
+                    .interface
+                    .type_defs()
+                    .iter()
+                    .all(|dt| dt.name() != &variant_name)
+                {
+                    self.interface.type_def({
+                        let cases = cases
+                            .into_iter()
+                            .map(|(name, case)| wit_encoder::VariantCase::value(name, case))
+                            .collect_vec();
+                        let variant = wit_encoder::TypeDef::variant(variant_name.clone(), cases);
+                        variant
+                    });
+                }
+
+                Ok(wit_encoder::Type::named(variant_name))
             }
-
-            Ok(wit_encoder::Type::named(variant_name))
         }
     }
-}
 
-// WebIdl non any 2 Wit
-fn wi_non_any2w(
-    interface: &mut wit_encoder::Interface,
-    wi: &weedle::types::NonAnyType,
-    optional: bool,
-) -> anyhow::Result<wit_encoder::Type> {
-    let type_ = match wi {
-        weedle::types::NonAnyType::Boolean(_) => wit_encoder::Type::Bool,
-        weedle::types::NonAnyType::ByteString(_) => wit_encoder::Type::String,
-        weedle::types::NonAnyType::DOMString(_) => wit_encoder::Type::String,
-        weedle::types::NonAnyType::USVString(_) => wit_encoder::Type::String,
-        weedle::types::NonAnyType::Integer(int) => match int.type_ {
-            weedle::types::IntegerType::LongLong(int) => match int.unsigned {
-                Some(_) => wit_encoder::Type::U64,
-                None => wit_encoder::Type::S64,
+    // WebIdl non any 2 Wit
+    fn wi_non_any2w(
+        // interface: &mut wit_encoder::Interface,
+        &mut self,
+        wi: &weedle::types::NonAnyType,
+        optional: bool,
+    ) -> anyhow::Result<wit_encoder::Type> {
+        let type_ = match wi {
+            weedle::types::NonAnyType::Boolean(_) => wit_encoder::Type::Bool,
+            weedle::types::NonAnyType::ByteString(_) => wit_encoder::Type::String,
+            weedle::types::NonAnyType::DOMString(_) => wit_encoder::Type::String,
+            weedle::types::NonAnyType::USVString(_) => wit_encoder::Type::String,
+            weedle::types::NonAnyType::Integer(int) => match int.type_ {
+                weedle::types::IntegerType::LongLong(int) => match int.unsigned {
+                    Some(_) => wit_encoder::Type::U64,
+                    None => wit_encoder::Type::S64,
+                },
+                weedle::types::IntegerType::Long(int) => match int.unsigned {
+                    Some(_) => wit_encoder::Type::U32,
+                    None => wit_encoder::Type::S32,
+                },
+                weedle::types::IntegerType::Short(int) => match int.unsigned {
+                    Some(_) => wit_encoder::Type::U16,
+                    None => wit_encoder::Type::S16,
+                },
             },
-            weedle::types::IntegerType::Long(int) => match int.unsigned {
-                Some(_) => wit_encoder::Type::U32,
-                None => wit_encoder::Type::S32,
+            weedle::types::NonAnyType::FloatingPoint(float) => match float.type_ {
+                weedle::types::FloatingPointType::Float(_) => wit_encoder::Type::F32,
+                weedle::types::FloatingPointType::Double(_) => wit_encoder::Type::F64,
             },
-            weedle::types::IntegerType::Short(int) => match int.unsigned {
-                Some(_) => wit_encoder::Type::U16,
-                None => wit_encoder::Type::S16,
-            },
-        },
-        weedle::types::NonAnyType::FloatingPoint(float) => match float.type_ {
-            weedle::types::FloatingPointType::Float(_) => wit_encoder::Type::F32,
-            weedle::types::FloatingPointType::Double(_) => wit_encoder::Type::F64,
-        },
-        weedle::types::NonAnyType::Identifier(ident) => {
-            wit_encoder::Type::named(ident_name(ident.type_.0))
-        }
-        weedle::types::NonAnyType::Promise(promise) => {
-            // use wit_encoder::TypeDefKind::Future instead?
-            match &*promise.generics.body {
-                weedle::types::ReturnType::Undefined(_) => todo!(),
-                weedle::types::ReturnType::Type(type_) => wi2w_type(interface, type_, false)?,
+            weedle::types::NonAnyType::Identifier(ident) => {
+                wit_encoder::Type::named(ident_name(ident.type_.0))
             }
-        }
-        weedle::types::NonAnyType::Sequence(seq) => {
-            let type_ = wi2w_type(interface, &*seq.type_.generics.body, seq.q_mark.is_some())?;
-            wit_encoder::Type::list(type_)
-        }
-        weedle::types::NonAnyType::Error(_) => todo!(),
-        weedle::types::NonAnyType::Byte(_) => todo!(),
-        weedle::types::NonAnyType::Octet(_) => todo!(),
-        weedle::types::NonAnyType::Object(_) => todo!(),
-        weedle::types::NonAnyType::Symbol(_) => todo!(),
-        weedle::types::NonAnyType::ArrayBuffer(_) => todo!(),
-        weedle::types::NonAnyType::DataView(_) => todo!(),
-        weedle::types::NonAnyType::Int8Array(_) => todo!(),
-        weedle::types::NonAnyType::Int16Array(_) => todo!(),
-        weedle::types::NonAnyType::Int32Array(_) => todo!(),
-        weedle::types::NonAnyType::Uint8Array(_) => todo!(),
-        weedle::types::NonAnyType::Uint16Array(_) => todo!(),
-        weedle::types::NonAnyType::Uint32Array(_) => todo!(),
-        weedle::types::NonAnyType::Uint8ClampedArray(_) => todo!(),
-        weedle::types::NonAnyType::Float32Array(_) => todo!(),
-        weedle::types::NonAnyType::Float64Array(_) => todo!(),
-        weedle::types::NonAnyType::ArrayBufferView(_) => todo!(),
-        weedle::types::NonAnyType::BufferSource(_) => todo!(),
-        weedle::types::NonAnyType::FrozenArrayType(_) => todo!(),
-        weedle::types::NonAnyType::RecordType(_) => todo!(),
-    };
+            weedle::types::NonAnyType::Promise(promise) => {
+                // use wit_encoder::TypeDefKind::Future instead?
+                match &*promise.generics.body {
+                    weedle::types::ReturnType::Undefined(_) => todo!(),
+                    weedle::types::ReturnType::Type(type_) => self.wi2w_type(type_, false)?,
+                }
+            }
+            weedle::types::NonAnyType::Sequence(seq) => {
+                let type_ = self.wi2w_type(&*seq.type_.generics.body, seq.q_mark.is_some())?;
+                wit_encoder::Type::list(type_)
+            }
+            weedle::types::NonAnyType::Error(_) => todo!(),
+            weedle::types::NonAnyType::Byte(_) => todo!(),
+            weedle::types::NonAnyType::Octet(_) => todo!(),
+            weedle::types::NonAnyType::Object(_) => todo!(),
+            weedle::types::NonAnyType::Symbol(_) => todo!(),
+            weedle::types::NonAnyType::ArrayBuffer(_) => todo!(),
+            weedle::types::NonAnyType::DataView(_) => todo!(),
+            weedle::types::NonAnyType::Int8Array(_) => todo!(),
+            weedle::types::NonAnyType::Int16Array(_) => todo!(),
+            weedle::types::NonAnyType::Int32Array(_) => todo!(),
+            weedle::types::NonAnyType::Uint8Array(_) => todo!(),
+            weedle::types::NonAnyType::Uint16Array(_) => todo!(),
+            weedle::types::NonAnyType::Uint32Array(_) => todo!(),
+            weedle::types::NonAnyType::Uint8ClampedArray(_) => todo!(),
+            weedle::types::NonAnyType::Float32Array(_) => todo!(),
+            weedle::types::NonAnyType::Float64Array(_) => todo!(),
+            weedle::types::NonAnyType::ArrayBufferView(_) => todo!(),
+            weedle::types::NonAnyType::BufferSource(_) => todo!(),
+            weedle::types::NonAnyType::FrozenArrayType(_) => todo!(),
+            weedle::types::NonAnyType::RecordType(_) => todo!(),
+        };
 
-    Ok(match optional {
-        false => type_,
-        true => wit_encoder::Type::option(type_),
-    })
+        Ok(match optional {
+            false => type_,
+            true => wit_encoder::Type::option(type_),
+        })
+    }
 }
 
 fn clean_generic(mut s: String) -> String {
