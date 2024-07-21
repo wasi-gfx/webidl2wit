afae04db2254764c20e98dae87a059dd164606e8
Mendy Berger <12537668+MendyBerger@users.noreply.github.com>
2024-07-21 11:13:09 -0400

    Support inheritance

--- src/lib.rs
+++ src/lib.rs
@@ -2,5 +2,5 @@ mod custom_resources;
 mod translations;
 mod types_;
 
-pub use translations::{webidl_to_wit, ConversionOptions, HandleUnsupported};
+pub use translations::{webidl_to_wit, ConversionOptions, HandleUnsupported, ResourceInheritance};
 pub use wit_encoder::{Ident, PackageName};
